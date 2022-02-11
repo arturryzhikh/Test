09e86bdc2a577949348c5e763207c61eda90de6c
arturryzhikh <ryzhiha@gmail.com>
2022-02-11 09:38:24 +0300

    make cell in main sb

--- Test/Reusable Views/RoundedButton.swift
+++ Test/Reusable Views/RoundedButton.swift
@@ -6,32 +6,17 @@
 //
 
 import UIKit
-@IBDesignable class RoundedButton: UIButton {
-    
-    @IBInspectable var cornerRadius: Int {
-        get {
-            return Int(layer.cornerRadius)
-        }
-        set {
-            self.layer.cornerRadius = CGFloat(newValue)
-        }
+
+class RoundedButton: UIButton {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
     }
-    
-    @IBInspectable var borderColor: UIColor {
-        get {
-            return UIColor(cgColor: self.layer.borderColor ?? UIColor.gray.cgColor)
-        }
-        
-        set {
-            self.layer.borderColor = newValue.cgColor
-        }
-    }
-    
     override func layoutSubviews() {
         super.layoutSubviews()
-        layer.borderColor = borderColor.cgColor
-        layer.cornerRadius = CGFloat(cornerRadius)
-        layer.borderWidth = 2.0
+        layer.cornerRadius = 30
+        layer.borderColor = titleLabel?.textColor.cgColor
+        layer.borderWidth = 1.0
         layer.masksToBounds = true
     }
 }
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -21,8 +21,8 @@ class ViewController: UIViewController {
        
         tableView.sectionHeaderHeight = UITableView.automaticDimension
         tableView.sectionFooterHeight = UITableView.automaticDimension
-        tableView.register(UINib(nibName: PersonCell.className, bundle: nil), forCellReuseIdentifier: PersonCell.className)
-        tableView.register(UINib(nibName: PersonHeader.className, bundle: nil), forHeaderFooterViewReuseIdentifier: PersonHeader.className)
+//        tableView.register(UINib(nibName: PersonCell.className, bundle: nil), forCellReuseIdentifier: PersonCell.className)
+//        tableView.register(UINib(nibName: PersonHeader.className, bundle: nil), forHeaderFooterViewReuseIdentifier: PersonHeader.className)
         tableView.register(UINib(nibName: PersonFooter.className, bundle: nil), forHeaderFooterViewReuseIdentifier: PersonFooter.className)
       }
 
@@ -36,16 +36,12 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PersonCell.className, for: indexPath) as! PersonCell
-        cell.ageTextField.delegate = self
-        cell.nameTextField.delegate = self
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath)
+//        cell.ageTextField.delegate = self
+//        cell.nameTextField.delegate = self
         return cell
     }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: PersonHeader.className)
-        return header
-    }
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let footer = tableView.dequeueReusableHeaderFooterView(withIdentifier: PersonFooter.className)
