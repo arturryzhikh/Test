2c6042850de19174488feddc8de4806672f2e7d1
arturryzhikh <ryzhiha@gmail.com>
2022-02-12 14:27:29 +0300

    configure cells with viewModel

--- Test/Views/ViewController.swift
+++ Test/Views/ViewController.swift
@@ -17,15 +17,19 @@ class ViewController: UIViewController {
         let actionSheet = UIAlertController(title: nil,
                                             message: nil,
                                             preferredStyle: .actionSheet)
+        
         let clearAction = UIAlertAction(title: "Сбросить данные",
                                         style: .default) { [weak self ] action -> Void in
             guard let self = self else { return }
             self.viewModel.clear()
         }
         
-        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel) { action -> Void in
+        let cancelAction = UIAlertAction(title: "Отмена",
+                                         style: .cancel) { action -> Void in
+            
             return
         }
+        
         actionSheet.addAction(clearAction)
         actionSheet.addAction(cancelAction)
         present(actionSheet, animated: true,completion: nil)
@@ -69,9 +73,15 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let section = ViewModel.Sections(rawValue: indexPath.section)
         switch section {
         case .person:
-            let cell = tableView.dequeueReusableCell(withIdentifier: PersonCell.className, for: indexPath) as! PersonCell
-            cell.ageTextField.text = viewModel.person.age
-            cell.nameTextField.text = viewModel.person.name
+            let cell = tableView
+                .dequeueReusableCell(withIdentifier: PersonCell.className, for: indexPath) as! PersonCell
+            
+            if let parent = viewModel.pickPerson(at: indexPath) {
+                cell.ageTextField.text = parent.age
+                cell.nameTextField.text = parent.name
+            }
+            
+            
             cell.nameChanged = { [weak self] name in
                 guard let self = self else { return }
                 self.viewModel.setName(name,at: indexPath)
@@ -85,12 +95,14 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
             return cell
             
         case .children:
-            let cell = tableView.dequeueReusableCell(withIdentifier: ChildCell.className, for: indexPath) as! ChildCell
+            let cell = tableView
+                .dequeueReusableCell(withIdentifier: ChildCell.className, for: indexPath) as! ChildCell
+            
             if let child = viewModel.pickPerson(at: indexPath) {
                 cell.ageTextField.text = child.age
                 cell.nameTextField.text = child.name
             }
-           
+            
             
             cell.nameChanged = { [weak self] name in
                 guard let self = self else { return }
@@ -115,12 +127,14 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let footer = tableView.dequeueReusableHeaderFooterView(withIdentifier: PersonFooter.className) as! PersonFooter
+        let footer = tableView
+            .dequeueReusableHeaderFooterView(withIdentifier: PersonFooter.className) as! PersonFooter
         
         footer.addChildAction = { [weak self] in
             guard let self = self else { return }
             self.viewModel.addChild()
         }
+        
         let section = ViewModel.Sections(rawValue: section)
         switch section {
         case .person:
